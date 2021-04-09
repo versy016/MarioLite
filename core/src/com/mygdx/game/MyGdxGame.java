0471047ca59a5d167a889d1e7dd7c9b92a5896ae
shivam verma <versy016@mymail.unisa.edu.au>
2021-04-10 02:02:57 +0930

    added menu background

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,33 +1,33 @@
 package com.mygdx.game;
-
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends Game implements ApplicationListener {
 	SpriteBatch batch;
 	Texture img;
-	
+	// The class with the menu
+	public static menuclass mclass;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Gdx.app.log("MyGdxGame: "," create");
+		mclass = new menuclass(this);
+		Gdx.app.log("MyGdxGame: ","about to change screen to menuScreen");
+		setScreen(mclass);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+	super.render();}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.dispose();
 	}
 }
--- core/src/com/mygdx/game/menuclass.java
+++ core/src/com/mygdx/game/menuclass.java
@@ -1,16 +1,43 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class menuclass implements Screen {
+    MyGdxGame game;
+    private Texture background;
+    private SpriteBatch batch;
+    private Stage stage;
+    //background = new Texture("background.jpg");
+   // final TextButton button = new TextButton("Click me",, "default");
+    // constructor to keep a reference to the main Game class
+    public menuclass(MyGdxGame game) {
+        this.game = game;
+        background = new Texture("Starting Assets/assets/background.jpg");
+    }
+    public void create() {
+        batch = new SpriteBatch();
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+    }
+
     @Override
     public void show() {
-
+        Gdx.app.log("MenuScreen: ","menuScreen show called");
+        create();
     }
 
     @Override
     public void render(float delta) {
 
+        stage.getBatch().begin();
+        stage.getBatch().draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        stage.getBatch().end();
     }
 
     @Override
