25c7a5cd4276f6b43337a1dc1c62f53b558505f6
shivam verma <versy016@mymail.unisa.edu.au>
2021-04-12 02:13:02 +0930

    camera added

---
+++ android/assets/Starting Assets/assets/Items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Items" tilewidth="70" tileheight="70" tilecount="128" columns="4">
+ <image source="Items.png" width="280" height="140"/>
+</tileset>
